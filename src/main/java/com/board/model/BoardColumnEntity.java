1030476e1f00f921298c761c0d7ca9d2025cb1c2
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-04-10 11:27:09 -0300

    creating boardcolumnentity

--- src/main/java/com/board/model/BoardColumnEntity.java
+++ src/main/java/com/board/model/BoardColumnEntity.java
@@ -2,4 +2,42 @@ package com.board.model;
 
 public class BoardColumnEntity {
 
+	private Long id;
+    private String name;
+    private int order;
+    private BoardColumnKindEnum kind;
+    private BoardEntity board;
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getOrder() {
+		return order;
+	}
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	public BoardColumnKindEnum getKind() {
+		return kind;
+	}
+	public void setKind(BoardColumnKindEnum kind) {
+		this.kind = kind;
+	}
+	public BoardEntity getBoard() {
+		return board;
+	}
+	public void setBoard(BoardEntity board) {
+		this.board = board;
+	}
+    
+    
 }
