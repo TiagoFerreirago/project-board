0eda2bcb3c64a860e51286f54753d74b337c6788
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-04-13 16:03:54 -0300

    adding converter timestamp

--- src/main/java/com/board/converter/OffsetDateTimeConverter.java
+++ src/main/java/com/board/converter/OffsetDateTimeConverter.java
@@ -2,7 +2,6 @@ package com.board.converter;
 
 import java.sql.Timestamp;
 import java.time.OffsetDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.util.Objects;
 
@@ -12,5 +11,10 @@ public final class OffsetDateTimeConverter {
 	
 	public static OffsetDateTime toOffsetDateTime(Timestamp time) {
 		return Objects.nonNull(time)?OffsetDateTime.ofInstant(time.toInstant(), ZoneOffset.UTC): null;
+	}
+	
+	public static Timestamp toTimeStamp(OffsetDateTime time) {
+
+		return Objects.nonNull(time) ? Timestamp.valueOf(time.atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()) : null;
 	}
 }
